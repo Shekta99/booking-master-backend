4f3bebc212d0a3ff16211651bf685beb2ff0ee55
Sergio Marentes <sergiomarentes705@gmail.com>
2022-08-17 10:28:32 -0500

    adding oprtions to preflight

--- functions/restaurants/restaurants.ts
+++ functions/restaurants/restaurants.ts
@@ -3,11 +3,21 @@ import { createRestaurant } from "./createRestaurants";
 import { readRestaurant } from "./readRestaurants";
 
 const handler: Handler = (event, context, callback) => {
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+  };
   switch (event.httpMethod) {
     case "GET":
       return readRestaurant(event, context, callback);
     case "POST":
       return createRestaurant(event, context, callback);
+    case "OPTIONS":
+      const allowCors: Handler = async (context, event) => {
+        return { statusCode: 200, headers };
+      };
+      return allowCors(event, context, callback);
     default:
       return new Promise((resolve) => {
         resolve({ statusCode: 501 });
