fb464366ffe4e57e19ffa6d900058748e7479527
Sergio Marentes <sergiomarentes705@gmail.com>
2023-05-18 09:56:54 -0500

    adding avaliabily

--- models/AdvertisementModel.ts
+++ models/AdvertisementModel.ts
@@ -4,12 +4,14 @@ export interface Advertisement {
   name: string;
   imageURL: string;
   speciality: string;
+  availability: boolean;
 }
 
 const schema = new Schema<Advertisement>({
   name: { type: String, required: true },
   imageURL: { type: String, required: true },
   speciality: { type: String, required: true },
+  availability: { type: Boolean, required: true },
 });
 
 export const AdvertisementModel = model<Advertisement>(
